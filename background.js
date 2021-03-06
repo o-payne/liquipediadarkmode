076ceb275a2164d76f4e8acc8f95b6a8c2a606aa
Owen <owen.payne42@gmail.com>
2021-03-06 04:38:06 -0500

    initial variable of dark wasnt defined

--- background.js
+++ background.js
@@ -1,30 +1,35 @@
 console = chrome.extension.getBackgroundPage().console;
 
+chrome.storage.sync.set({ 'dark': false }, function () {
+  // Notify that we saved.
+  console.log('Initial value set ');
+});
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === 'loading') {
-    chrome.storage.sync.get(['dark'], function (result) {
-      if (result.dark === true) {
-        chrome.tabs.insertCSS(null, { file: "dark_mode.css" });
-        chrome.storage.sync.set({ 'dark': true }, function () {
-          // Notify that we saved.
-          console.log('Dark enabled ' + result.dark);
-        });
-      }
+  //if (changeInfo.status === 'loading') {
+  chrome.storage.sync.get(['dark'], function (result) {
+    if (result.dark === true) {
+      chrome.tabs.insertCSS(null, { file: "dark_mode.css" });
+      chrome.storage.sync.set({ 'dark': true }, function () {
+        // Notify that we saved.
+        console.log('Dark enabled ' + result.dark);
+      });
+    }
 
-      else if (result.dark === false) {
-        chrome.tabs.removeCSS(null, { file: "dark_mode.css" });
-        chrome.storage.sync.set({ 'dark': false }, function () {
-          // Notify that we saved.
-          console.log('Dark enabled ' + result.dark);
-        });
-      }
-    })
-  }
+    else if (result.dark === false) {
+      chrome.tabs.removeCSS(null, { file: "dark_mode.css" });
+      chrome.storage.sync.set({ 'dark': false }, function () {
+        // Notify that we saved.
+        console.log('Dark enabled ' + result.dark);
+      });
+    }
+  })
+  //}
 }
 )
 
 chrome.browserAction.onClicked.addListener(function (tab) {
+
   chrome.storage.sync.get(['dark'], function (result) {
     if (result.dark === false) {
       chrome.tabs.insertCSS(null, { file: "dark_mode.css" });
